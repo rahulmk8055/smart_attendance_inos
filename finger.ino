ee80de630e0023e7a69761969162e0ed37509772
rahulmk8055 <40895295+rahulmk8055@users.noreply.github.com>
2019-04-12 11:09:25 +0530

    Add files via upload

---
+++ finger.ino
@@ -0,0 +1,110 @@
+#include "FPS_GT511C3.h"
+#include "SoftwareSerial.h"
+
+// Hardware setup - FPS connected to:
+//    digital pin 4(arduino rx, fps tx)
+//    digital pin 5(arduino tx - 560ohm resistor - fps tx - 1000ohm resistor - ground)
+//    this voltage divider brings the 5v tx line down to about 3.2v so we dont fry our fps
+
+FPS_GT511C3 fps(10, 11);
+//Serialb send(6,7);
+
+bool start = true;
+
+void setup()
+{
+
+  Serial.begin(9600);
+  Serial1.begin(115200);
+//  send.begin(9600);
+  delay(100);
+  fps.Open();
+  fps.SetLED(true);
+}
+void Enroll()
+{
+  // Enroll test
+
+  // find open enroll id
+  int enrollid = 0;
+  bool usedid = true;
+  while (usedid == true)
+  {
+    usedid = fps.CheckEnrolled(enrollid);
+    if (usedid==true) enrollid++;
+  }
+  fps.EnrollStart(enrollid);
+
+  // enroll
+  Serial.print("Press finger to Enroll #");
+  Serial.println(enrollid);
+  while(fps.IsPressFinger() == false) delay(100);
+  bool bret = fps.CaptureFinger(true);
+  int iret = 0;
+  if (bret != false)
+  {
+    Serial.println("Remove finger");
+    fps.Enroll1(); 
+    while(fps.IsPressFinger() == true) delay(100);
+    Serial.println("Press same finger again");
+    while(fps.IsPressFinger() == false) delay(100);
+    bret = fps.CaptureFinger(true);
+    if (bret != false)
+    {
+      Serial.println("Remove finger");
+      fps.Enroll2();
+      while(fps.IsPressFinger() == true) delay(100);
+      Serial.println("Press same finger yet again");
+      while(fps.IsPressFinger() == false) delay(100);
+      bret = fps.CaptureFinger(true);
+      if (bret != false)
+      {
+        Serial.println("Remove finger");
+        iret = fps.Enroll3();
+        if (iret == 0)
+        {
+          Serial.println("Enrolling Successful");
+        }
+        else
+        {
+          Serial.print("Enrolling Failed with error code:");
+          Serial.println(iret);
+        }
+      }
+      else Serial.println("Failed to capture third finger");
+    }
+    else Serial.println("Failed to capture second finger");
+  }
+  else Serial.println("Failed to capture first finger");
+}
+
+
+void loop()
+{
+  if(start){
+    Enroll();
+    start = false;//begin enrolling fingerprint
+  }
+  // Identify fingerprint test
+//while
+  if (fps.IsPressFinger())
+  {
+    fps.CaptureFinger(false);
+    int id = fps.Identify1_N();
+    if (id <200)
+    {
+      Serial.print("Verified ID:");
+      Serial.println(id);
+      Serial1.println(id);
+    }
+    else
+    {
+      Serial.println("Finger not found");
+    }
+  }
+  else
+  {
+    Serial.println("Please press finger");
+  }
+  delay(100);
+}
